9b2449171e0de37a7b7ca0299cb182154e3dfa97
Leonardo Freitas <leonardofreitas159@gmail.com>
2020-01-28 21:45:31 -0300

    função lista

--- commands.py
+++ commands.py
@@ -1,6 +1,7 @@
 # commands
 EXIT = ['exit', '-e']
 CREATE = ['create', '-c']
+LIST = ['list', '-l']
 
 # ENTITIES
 PARTICIPANTS = ['participants', '-p']
--- controllers/participant_controller.py
+++ controllers/participant_controller.py
@@ -11,11 +11,29 @@ path_file = path_connection + '/files/participants.txt'
 def route(command):
     if command in commands.CREATE:
         create()
+    if command in commands.LIST:
+        list_all()
 
 
 def create():
     name = input('Nome do participante $>> ')
+    index = str(get_next_index())
     with open(path_file, 'a') as file:
-        name_linebreak = '{name}\n'.format(name=name)
+        name_linebreak = '{index} {name}\n'.format(index=index, name=name)
         file.write(name_linebreak)
     print('Participante adicionado com sucesso!')
+
+
+def list_all():
+    with open(path_file, 'r') as file:
+        for participant in file.readlines():
+            p = participant.split(' ')
+            print('{index} => {name}'.format(index=p[0], name=p[1][:-1]))
+
+
+def get_next_index():
+    with open(path_file, 'r') as file:
+        lines = file.readlines()
+        if lines:
+            return int(lines[-1].split(' ')[0]) + 1
+        return 1
